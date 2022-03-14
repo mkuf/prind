2eeeebb15f9d1a57ee74c780500d54e70e0bd7e1
Markus Küffner <kueffner.markus@gmail.com>
2022-03-14 22:11:10 +0100

    script: fix user creation and package installation for X11

--- scripts/setup-X11.sh
+++ scripts/setup-X11.sh
@@ -2,23 +2,20 @@
 ## xterm is running in foreground until the klipperscreen
 ## container connects to the xserver
 
-#!/bin/bash -xe
+#!/bin/bash
+
+set -xe
 
 ## Name of the new user
 USER=screen
 
 ## Create User
-adduser --system --disabled-password --no-create-home ${USER}
+adduser --system --disabled-password --no-create-home --gecos '' --shell /bin/bash ${USER}
+usermod -a -G tty ${USER}
 
 ## Install Packages
 apt update
-apt install -y
-  xterm
-  xinit
-  xinput
-  xserver-xorg
-  x11-xserver-utils
-  xserver-xorg-video-fbdev
+apt install -y xterm xinit xinput xserver-xorg x11-xserver-utils xserver-xorg-video-fbdev
 
 ## Allow any User to start X
 sed -i 's/allowed_users=console/allowed_users=anybody/g' /etc/X11/Xwrapper.config || true
